d408d463345f37cf1736fa4bd604ecf9291e7bb1
Adriano Vinhas <vinhas.adriano@gmail.com>
2022-09-17 14:07:18 +0100

    make mnist and fashion-mnist the only datasets available to play with

--- f-denser/fast_denser/misc/constants.py
+++ f-denser/fast_denser/misc/constants.py
@@ -1,7 +1,8 @@
 from typing import List, Tuple
 
-VALID_DATASET_NAMES: List[str] = ["mnist", "fashion-mnist", "svhn", "cifar10",
-                                  "cifar100-fine", "cifar100-coarse", "tiny-imagenet"]
+VALID_DATASET_NAMES: List[str] = ["mnist", "fashion-mnist"]
+#, "svhn", "cifar10",
+# "cifar100-fine", "cifar100-coarse", "tiny-imagenet"]
 INPUT_DIMENSIONS: Tuple[int, int, int] = (1, 32, 32)
 
 OVERALL_BEST_FOLDER = "overall_best"
