f9ca6f722fbed762e2acc68ead129d91a09a1baa
Adriano Vinhas <vinhas.adriano@gmail.com>
2022-09-17 14:10:05 +0100

    simplify data split process

--- f-denser/fast_denser/neural_networks_torch/dataset_loader.py
+++ f-denser/fast_denser/neural_networks_torch/dataset_loader.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
 from enum import unique, Enum
-from math import floor
-from typing import Dict, List, Tuple, TYPE_CHECKING
+from typing import Any, Dict, List, Tuple, TYPE_CHECKING
 
 from fast_denser.neural_networks_torch.transformers import BaseTransformer
 
-import numpy as np
+from sklearn.model_selection import train_test_split
 from torch.utils.data import Subset
 from torchvision import datasets
 
@@ -24,39 +23,39 @@ class DatasetType(Enum):
     TEST = "test"
 
 
+
 def load_dataset(dataset_name: str,
                  train_transformer: BaseTransformer,
-                 test_transformer: BaseTransformer) -> Dict[DatasetType, Dataset]:
+                 test_transformer: BaseTransformer,
+                 enable_stratify: bool) -> Dict[DatasetType, Dataset]:
     train_data: Dataset
-    train_data_for_fitness: Dataset
     test_data: Dataset
     if dataset_name == "fashion-mnist":
-        train_data, train_data_for_fitness, test_data = _load_fashion_mnist(train_transformer, test_transformer)
+        train_data, test_data = _load_fashion_mnist(train_transformer, test_transformer)
     elif dataset_name == "mnist":
-        train_data, train_data_for_fitness, test_data = _load_mnist(train_transformer, test_transformer)
+        train_data, test_data = _load_mnist(train_transformer, test_transformer)
     else:
         raise ValueError(f"Invalid dataset name: {dataset_name}")
 
-    #print(type(train_data))
-    #print(train_data.targets)
-    #print(train_data.data)
-    #import sys
-    #sys.exit(0)
-    train_size: int = len(train_data) # type: ignore
-    aug_factor: int = 1 # 4 is thrown randomly
-    train_indices: List[int] = list(range(train_size)) * aug_factor 
-    real_train_size: int = train_size * aug_factor
-    np.random.shuffle(train_indices)
-    evo_train_end_idx: int = floor(0.7 * train_size)
-    evo_validation_start_idx: int = evo_train_end_idx
-    evo_validation_end_idx: int = evo_train_end_idx + floor(0.2 * train_size)
-    evo_test_start_idx: int = evo_validation_end_idx
+    targets: List[Any] = train_data.targets # type: ignore
+    train_indices: List[int] = list(range(len(targets))) # * aug_factor
+
+    stratify: Any 
+    stratify = targets if enable_stratify is True else None
+    train_idx, test_idx = train_test_split(train_indices,
+                                           test_size=0.2,
+                                           shuffle=True,
+                                           stratify=stratify)
+    stratify = targets[train_idx] if enable_stratify is True else None
+    train_idx, valid_idx = train_test_split(train_idx,
+                                            test_size=0.25,
+                                            shuffle=True,
+                                            stratify=stratify) # 0.25 x 0.8 = 0.2
     
-    evo_train_subset: Subset = Subset(train_data, train_indices[:evo_train_end_idx])
-    evo_validation_subset: Subset = Subset(train_data,
-                                           train_indices[evo_validation_start_idx:evo_validation_end_idx])
-    evo_test_subset: Subset = Subset(train_data_for_fitness, train_indices[evo_test_start_idx:])
-    
+    evo_train_subset: Subset = Subset(train_data, train_idx)
+    evo_validation_subset: Subset = Subset(train_data, valid_idx)
+    evo_test_subset: Subset = Subset(train_data, test_idx)
+
     return {
         DatasetType.EVO_TRAIN: evo_train_subset,
         DatasetType.EVO_VALIDATION: evo_validation_subset,
@@ -66,18 +65,12 @@ def load_dataset(dataset_name: str,
 
 
 def _load_fashion_mnist(train_transformer: BaseTransformer,
-                        test_transformer: BaseTransformer) -> Tuple[Dataset, Dataset, Dataset]:
+                        test_transformer: BaseTransformer) -> Tuple[Dataset, Dataset]:
     train_data = datasets.FashionMNIST(
         root="data",
         train=True,
         download=True,
         transform=train_transformer
-    )
-    train_data_for_fitness = datasets.FashionMNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=test_transformer
     )
     test_data = datasets.FashionMNIST(
         root="data",
@@ -86,21 +79,15 @@ def _load_fashion_mnist(train_transformer: BaseTransformer,
         transform=test_transformer
     )
 
-    return train_data, train_data_for_fitness, test_data
+    return train_data, test_data
 
 def _load_mnist(train_transformer: BaseTransformer,
-                test_transformer: BaseTransformer) -> Tuple[Dataset, Dataset, Dataset]:
+                test_transformer: BaseTransformer) -> Tuple[Dataset, Dataset]:
     train_data = datasets.MNIST(
         root="data",
         train=True,
         download=True,
         transform=train_transformer
-    )
-    train_data_for_fitness = datasets.MNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=test_transformer
     )
     test_data = datasets.MNIST(
         root="data",
@@ -108,4 +95,4 @@ def _load_mnist(train_transformer: BaseTransformer,
         download=True,
         transform=test_transformer
     )
-    return train_data, train_data_for_fitness, test_data
+    return train_data, test_data
