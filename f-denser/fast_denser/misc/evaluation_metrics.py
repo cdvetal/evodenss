6af819d5f027e943435a1baccf672064e67b695c
Adriano Vinhas <vinhas.adriano@gmail.com>
2023-02-05 15:42:52 +0000

    add new field to log

--- f-denser/fast_denser/misc/evaluation_metrics.py
+++ f-denser/fast_denser/misc/evaluation_metrics.py
@@ -16,6 +16,7 @@ class EvaluationMetrics:
     losses: Dict[str, List[float]]
     n_epochs: int
     total_epochs_trained: int
+    max_epochs_reached: bool
 
     @classmethod
     def default(cls, fitness: Fitness) -> "EvaluationMetrics":
@@ -28,7 +29,8 @@ class EvaluationMetrics:
             training_time_spent=0.0,
             losses={},
             n_epochs=0,
-            total_epochs_trained=0
+            total_epochs_trained=0,
+            max_epochs_reached=False
         )
 
     @classmethod
@@ -49,13 +51,20 @@ class EvaluationMetrics:
             f"total_epochs_trained: {self.total_epochs_trained},  " + \
             f"accuracy: {round(self.accuracy, 5) if self.accuracy is not None else self.accuracy},  " + \
             f"fitness: {self.fitness},  " + \
-            f"losses: {self.losses})"
+            f"losses: {self.losses}),  " + \
+            f"max_epochs_reached: {self.max_epochs_reached}"
 
     # To be used in case an individual gets extra training (through mutation or selection)
     def __add__(self, other: EvaluationMetrics) -> EvaluationMetrics:
 
         if self is None:
             return other
+
+        max_epochs_reached: bool
+        if self.max_epochs_reached is True or other.max_epochs_reached is True:
+            max_epochs_reached = True
+        else:
+            max_epochs_reached = False
 
         assert self.is_valid_solution == other.is_valid_solution
         assert self.n_trainable_parameters == other.n_trainable_parameters
@@ -74,5 +83,6 @@ class EvaluationMetrics:
             training_time_spent=self.training_time_spent + other.training_time_spent,
             n_epochs=other.n_epochs,
             total_epochs_trained=self.total_epochs_trained + other.n_epochs,
-            losses={k: self.losses[k] + other.losses[k] for k in self.losses.keys()}
+            losses={k: self.losses[k] + other.losses[k] for k in self.losses.keys()},
+            max_epochs_reached=max_epochs_reached
         )
